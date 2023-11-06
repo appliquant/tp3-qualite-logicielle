9b62af9e2ab418eeb705c9346b99805170a28b2f
Danny <ddeghmous@edu.cegepgarneau.ca>
2023-11-05 20:38:17 -0500

    Ajout commande "ShowAddClientPage" dans DashboardViewModel.

--- 14E_TP2_A23/ViewModels/DashboardViewModel.cs
+++ 14E_TP2_A23/ViewModels/DashboardViewModel.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using _14E_TP2_A23.Services;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,21 +12,32 @@ namespace _14E_TP2_A23.ViewModels
     /// <summary>
     /// View model de DashboardPage.xaml
     /// </summary>
-    public partial class DashboardViewModel
+    public partial class DashboardViewModel : ObservableRecipient
     {
         #region Propriétés
-
+        /// <summary>
+        /// Service de navigation injecté par le service provider
+        /// </summary>
+        private readonly IAppNavigationService? _appNavigtionService;
         #endregion
 
         #region Constructeur
-        public DashboardViewModel()
+        public DashboardViewModel(IAppNavigationService appNavigtionService)
         {
-
+            _appNavigtionService = appNavigtionService;
         }
 
         #endregion
 
         #region Commandes
+        [RelayCommand]
+        /// <summary>
+        /// Comande afficher la page inscription de client
+        /// </summary>
+        public void ShowAddClientPage()
+        {
+            _appNavigtionService?.NavigateTo("AddClientPage");
+        }
 
         #endregion
     }
