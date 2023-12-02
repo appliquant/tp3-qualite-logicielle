801c59ab5247a789d86a578410dce6b5fba6f7be
User <email@ema.com>
2023-12-02 00:22:54 -0500

    Ajout des modeles pour le gym d'escalade

---
+++ 14E_TP2_A23/Models/ClimbingRouteEvaluation.cs
@@ -0,0 +1,40 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System;
+
+namespace _14E_TP2_A23.Models
+{
+    /// <summary>
+    /// Représente une évaluation de la difficulté d'une voie d'escalade
+    /// </summary>
+    public class ClimbingRouteEvaluation
+    {
+        #region Propriétés
+        /// <summary>
+        /// Id de l'évaluation. Auto-généré par MongoDB
+        /// </summary>
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Note de l'évaluation
+        /// </summary>
+        [BsonElement("rating")]
+        public int Rating { get; set; }
+        #endregion
+
+        #region Constructeur
+        public ClimbingRouteEvaluation()
+        {
+
+        }
+
+        public ClimbingRouteEvaluation(int rating)
+        {
+            Rating = rating;
+        }
+
+        #endregion
+    }
+}
---
+++ 14E_TP2_A23/Models/ClimbingWall.cs
@@ -0,0 +1,53 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System;
+
+namespace _14E_TP2_A23.Models
+{
+    /// <summary>
+    /// Représente un mur d'escalade
+    /// </summary>
+    public class ClimbingWall
+    {
+        #region Propriétés
+        /// <summary>
+        /// Id du mur. Auto-généré par MongoDB
+        /// </summary>
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// L'emplacement du mur
+        /// </summary>
+        [BsonElement("location")]
+        public string Location { get; set; }
+
+        /// <summary>
+        /// Hauteur du mur
+        /// </summary>
+        [BsonElement("height")]
+        public int Height { get; set; }
+        #endregion
+
+        #region Constructeur
+        public ClimbingWall()
+        {
+
+        }
+
+        public ClimbingWall(string location, int height)
+        {
+            Location = location;
+            Height = height;
+        }
+        #endregion
+
+    }
+}
