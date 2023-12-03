878529b4ce8c332d5b36bef57eb375efe7ec0741
User <email@ema.com>
2023-12-03 02:00:28 -0500

    Afficher bouton "assigner a ce mur"

--- 14E_TP2_A23/Ressources/InverseBooleanToVisibilityConverter.cs
+++ 14E_TP2_A23/Ressources/InverseBooleanToVisibilityConverter.cs
@@ -7,19 +7,19 @@ namespace _14E_TP2_A23.Ressources
     /// <summary>
     /// Convertisseur de booléen en visibilité.
     /// Utilisé pour afficher ou non un élément en fonction de la valeur de la propriété.
-    /// Si booléen = true, l'élément est visible, sinon il est caché.
+    /// Si booléen = true, l'élément est caché, sinon il est visible.
     /// </summary>
-    public class BooleanToVisibilityConverter : IValueConverter
+    public class InverseBooleanToVisibilityConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (value is bool isBoolean)
             {
-                return isBoolean == true ? System.Windows.Visibility.Visible : System.Windows.Visibility.Collapsed;
+                return isBoolean == true ? System.Windows.Visibility.Collapsed : System.Windows.Visibility.Visible;
             }
             else
             {
-                return System.Windows.Visibility.Collapsed;
+                return System.Windows.Visibility.Visible;
             }
         }
 
